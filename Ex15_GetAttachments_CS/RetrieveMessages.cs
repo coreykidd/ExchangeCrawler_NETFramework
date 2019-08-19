ec2d8f660106405e43631fe54ab2d3d0fd021ba9
coreykidd <corey@coreykidd.com>
2019-08-18 22:27:03 -0500

    First commit. Can retrieve all messages in inbox. Playing with folder structures, and types besides messages.

---
+++ Ex15_GetAttachments_CS/RetrieveMessages.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace ExchangeCrawl
+{
+    class RetrieveMessages
+    {
+        static IUserData UserData = UserDataFromConsole.GetUserData();
+        static ExchangeService service = Service.ConnectToService(UserData, new TraceListener());
+
+        static void Main(string[] args)
+        {
+            //FindItemsResults<Item> items = GetMessages();
+            //foreach(Item i in items)
+            //{
+            //    Console.WriteLine(i.Subject);
+            //}
+            //Console.ReadLine();
+
+            Folder folder = GetFolder();
+            FindFoldersResults childFolders = folder.FindFolders(new FolderView(folder.ChildFolderCount));
+        }
+
+        public static FindItemsResults<Item> GetMessages()
+        {
+            ItemView view = new ItemView(10);
+            FindItemsResults<Item> results = service.FindItems(WellKnownFolderName.Inbox, view);
+            PropertySet propSet = new PropertySet(BasePropertySet.FirstClassProperties, EmailMessageSchema.TextBody);
+            service.LoadPropertiesForItems(results, propSet);
+            return results;
+        }
+
+        public static Folder GetFolder()
+        {
+            Folder rootfolder = Folder.Bind(service, WellKnownFolderName.MsgFolderRoot);
+            return rootfolder;
+        }
+    }
+}
