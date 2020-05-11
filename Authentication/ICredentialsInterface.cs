6f3dff0152026bef5f6fdadd48dc952d3d09401a
Corey Kidd <corey@coreykidd.com>
2020-05-11 17:23:12 -0500

    Created credentials interface.  It's hacky.

---
+++ Authentication/ICredentialsInterface.cs
@@ -0,0 +1,31 @@
+﻿using System.Security;
+
+namespace Authentication
+{
+    interface ICredentialsInterface
+    {
+        string EmailAddress { get; set; }
+        SecureString Password { get; set; }
+    }
+}
+
+//public class Credentials : ICredentialsInterface
+//{
+//    public string EmailAddress { get; set; }
+//    public SecureString Password { get; set; }
+
+//    public Credentials()
+//    {
+//        this.EmailAddress = "testuser";
+//        this.Password = new SecureString();
+//        this.Password.AppendChar('p');
+//        this.Password.AppendChar('a');
+//        this.Password.AppendChar('s');
+//        this.Password.AppendChar('s');
+//        this.Password.AppendChar('w');
+//        this.Password.AppendChar('o');
+//        this.Password.AppendChar('r');
+//        this.Password.AppendChar('d');
+//        this.Password.MakeReadOnly();
+//    }
+//}
